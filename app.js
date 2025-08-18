76f28e887079532b3e56a54b4ceafadb5b3af40a
Tomi <traggio2020@gmail.com>
2025-08-18 14:42:48 -0300

    Funciones implementadas

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Challenge 1
+// Funciones a Desarrollar Agregar Amigo, Sortear Amigo, Reset,
+
+let listaAmigos = [];
+const nombre = document.getElementById('amigo').value;    
+
+function agregarAmigo() {
+    const nombre = document.getElementById('amigo').value;
+
+    if (listaAmigos.includes(nombre)) {
+        alert("No puedes poner nombres repetidos");
+    } else if (nombre.trim() === "") {
+        alert("No puedes agregar un nombre vacío");
+    } else {
+        listaAmigos.push(nombre);
+        console.log(listaAmigos);
+        document.getElementById('amigo').value = '';
+        listarAmigos()
+    }
+}
+
+function listarAmigos() {
+    const listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+
+    let i = 0;
+    while (i < listaAmigos.length) {
+        const li = document.createElement('li');
+        li.textContent = listaAmigos[i]; 
+        listaHTML.appendChild(li);
+        i++;
+    }
+}
+function generarNumeroDeAmigoElegido() {
+    const personas = listaAmigos.length;
+    let numeroAleatorio = Math.floor(Math.random() * personas); 
+    console.log(numeroAleatorio);
+    return numeroAleatorio;
+}
+
+function sortearAmigo() {
+    const indice = generarNumeroDeAmigoElegido();
+    const amigoElegido = listaAmigos[indice];
+    mostrarResultado(amigoElegido);
+}
+
+function mostrarResultado(amigo) {
+    const resultadoHTML = document.getElementById('resultado');
+    resultadoHTML.textContent = "El amigo secreto es: " + amigo;
+    console.log("El amigo secreto es: " + amigo);
+}
